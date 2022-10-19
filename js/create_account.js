676ab641f0638d811d5adae31185ac0f6af4d173
JessHua159 <jesseweihuang@gmail.com>
2022-10-19 15:24:30 -0400

    save files before LF normalization

---
+++ js/create_account.js
@@ -0,0 +1,4 @@
+// If "Create Account" button is clicked
+    // Check that the fields have been correctly entered
+    // Take information from fields
+    // Send to "create account" route on Spring Boot
